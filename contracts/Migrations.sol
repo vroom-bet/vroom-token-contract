1e75399cb75a4a1c770688d12edcacf06ac51e3a
vroom <racing@vroom.bet>
2023-07-23 23:58:15 +0200

    doc(contract): add comment why we need this file

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// WE NEED THIS DUMMY FILE FOR TRUFFLE TO WORK PROPERLY WITH VYPER
+// OTHERWISE ARTIFACTS WILL NOT BE GENERATED PROPERLY
+
 contract Migrations {
     address public owner;
     uint public last_completed_migration;
