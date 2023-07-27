fef3d447e92ef29076ea7f51ef80f2da5fe182dc
vroom <racing@vroom.bet>
2023-07-27 12:24:23 +0200

    chore(deploy): team tokens transfers

--- migrations/2_transfer_team_tokens.js
+++ migrations/2_transfer_team_tokens.js
@@ -7,7 +7,7 @@ module.exports = async function (deployer, network, accounts) {
     // Transfer team tokens to DEV WALLET
     await vroom.transfer(
       "0x2e38856eB6F2a0aAF13cE7ce98e34901884c517C",
-      web3.utils.toWei(web3.utils.BN(300000000))
+      web3.utils.toWei(web3.utils.BN(126000000))
     );
   }
 };
